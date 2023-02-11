ee0ef5661a5117b93d8e0c54d35111212c070572
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-02-11 12:02:24 +0100

    fixing responsive design for smaller screens and chnanging the display to grid

--- src/components/VideosList/Video.js
+++ src/components/VideosList/Video.js
@@ -5,8 +5,9 @@ const Video = (props) => {
         <div className="video">
                         <a href={video.url} target="_blank" rel="noreferrer">
 
-                        <div className="video-thumbnail">
-                            <img src={video.img} alt={video.title} />
+                        <div className="video-thumbnail" style={{
+                            backgroundImage: `url(${video.img})`
+                        }}>
                             <p className="length">{video.length}</p>
                             <p className="hover">Maintenez la souris sur la vidéo pour lancer la lecture</p>
                         </div>
@@ -18,9 +19,20 @@ const Video = (props) => {
                                 <h3>{video.title}</h3>
                                 <a href={video.authorUrl} targget="_blank" rel="noreferrer">{video.author}</a>
                                 <p>{video.vues} vues . il y a {video.date}</p>
+                                
                             </div>
                         </div>
                         </a>
+                        <div className="video-info-hover">
+                                    <div>
+                                        <i class="bi bi-clock"></i>
+                                        <p>à regarder plus tard</p>
+                                    </div>
+                                    <div>
+                                        <i class="bi bi-list-check"></i>
+                                        <p>Ajouter à la file d'attente</p>
+                                    </div>
+                        </div>
         </div>
     )
 }
